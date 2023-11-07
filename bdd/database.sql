51e88a8903f3e041974f811914006cfa76dd437c
tbressel <tbressel.dev@gmail.com>
2023-11-07 13:57:00 +0100

    create sql queries to create database

---
+++ bdd/database.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS TODOLIST;
+
+CREATE DATABASE IF NOT EXISTS TODOLIST;
+
+USE TODOLIST;
+
+CREATE TABLE IF NOT EXISTS task (
+   id_task SMALLINT NOT NULL AUTO_INCREMENT,
+   name VARCHAR(50) NOT NULL,
+   description VARCHAR(50) NOT NULL,
+   remind_date DATE NOT NULL,
+   state TINYINT UNSIGNED,
+   priority SMALLINT NOT NULL,
+   PRIMARY KEY(Id_task)
+);
+
+
+
+INSERT INTO task (name, description, remind_date, state, priority)
+VALUES
+    (),
+
