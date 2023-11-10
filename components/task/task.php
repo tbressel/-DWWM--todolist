c17d15c0997cc077c28aafa9b2215dc1df082ad5
tbressel <tbressel.dev@gmail.com>
2023-11-10 09:06:18 +0100

    last push

--- components/task/task.php
+++ components/task/task.php
@@ -13,27 +13,28 @@ foreach ($taskList as $task) {
                     <div class="top__container--task_name">
                         <p>
                         <form action="components/task/task_actions.php?action=update&token=<?= $_SESSION['token'] ?>&id=<?= $task['id_task'] ?>" method="POST">
-                        
+
                             <input type="text" name="new_task_name" value="<?= $task['task_name'] ?>">
                             <input type="submit" value="Modifier">
                         </form>
                         </p>
                     </div>
                 </div>
-                <div class="bot__container">
-                    <ul class="task__action">
-                        <li class="btn">
-                            <a href="components/task/task_actions.php?action=delete&token=<?= $_SESSION['token'] ?>&id=<?= $task['id_task'] ?>">🗑️</a>
-                        </li>
-                    </ul>
-                </div>
+
             </div>
             <div class="right__container">
                 <div class="right__container--arrows">
                     <p class="btn up_btn">
-                        <a href="components/task/change_order.php?action=up&id=<?= $task['id_task'] ?>">⬆️</a></p>
+                        <a href="components/task/change_order.php?action=up&id=<?= $task['id_task'] ?>">⬆️</a>
+                    </p>
+                    <p class="task__action">
+                        <div class="btn">
+                            <a href="components/task/task_actions.php?action=delete&token=<?= $_SESSION['token'] ?>&id=<?= $task['id_task'] ?>">🗑️</a>
+                        </div>
+                    </p>
                     <p class="btn down_btn">
-                    <a href="components/task/change_order.php?action=down&id=<?= $task['id_task'] ?>">⬇️</a></p>
+                        <a href="components/task/change_order.php?action=down&id=<?= $task['id_task'] ?>">⬇️</a>
+                    </p>
                 </div>
             </div>
         </li>
