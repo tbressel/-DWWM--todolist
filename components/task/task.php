e26667b8425dfe824939a3f1a48147c7d965d18a
tbressel <tbressel.dev@gmail.com>
2023-11-12 20:25:58 +0100

    adding notification when tasks must be done today

--- components/task/task.php
+++ components/task/task.php
@@ -1,59 +1,69 @@
 <?php
+$actualDate = new DateTime('today');
+$actualDateString = $actualDate->format('Y-m-d');
 // $query = $connexion->prepare('SELECT * FROM task WHERE task_state = 0 ORDER BY id_task DESC');
 $query = $connexion->prepare('SELECT * FROM task WHERE task_state = 0 ORDER BY task_order DESC');
 $query->execute();
 $taskList = $query->fetchAll();
 if (!empty($taskList)) {
-foreach ($taskList as $task) {
+    foreach ($taskList as $task) {
+        
+        $taskDate = new DateTime($task['task_date']);
 ?>
-    <ul>
-        <li class="main__container" data-id="<?= $task['id_task'] ?>">
-            <div class="left__container">
-                <div class="top__container">
-                    <a href="components/task/task_actions.php?action=done&token=<?= $_SESSION['token'] ?>&id=<?= $task['id_task'] ?>"><span class="btn">☑️</span></a>
-                    <div class="top__container--task_name">
+        <ul>
+            <li class="main__container" data-id="<?= $task['id_task'] ?>">
 
+                <div class="left__container">
+                    <?php
+                    if ($taskDate == $actualDate) {
+                        echo '<div class="notification">ATTENTION il faut ' . $task['task_name'] . ' AUJOURD\'HUI </div>';
+                    } ?>
+                    <div class="top__container">
+                        <a href="components/task/task_actions.php?action=done&token=<?= $_SESSION['token'] ?>&id=<?= $task['id_task'] ?>"><span class="btn">☑️</span></a>
+                        <div class="top__container--task_name">
+
+                            <form action="components/task/task_actions.php" method="POST">
+                                <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
+                                <input type="hidden" name="id_task" value="<?= $task['id_task'] ?>">
+                                <input type="hidden" name="action" value="modify">
+                                <label for="new_task_name"></label>
+                                <input type="text" id="new_task_name" name="new_task_name" value="<?= $task['task_name'] ?>">
+                                <input type="submit" value="✒️">
+                            </form>
+                        </div>
+                    </div>
+                    <div class="remind_date">
                         <form action="components/task/task_actions.php" method="POST">
                             <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
                             <input type="hidden" name="id_task" value="<?= $task['id_task'] ?>">
-                            <input type="hidden" name="action" value="modify">
-                            <label for="new_task_name"></label>
-                            <input type="text" id="new_task_name" name="new_task_name" value="<?= $task['task_name'] ?>">
+                            <input type="hidden" name="action" value="date">
+                            <label for="new_date">Date de rappel:</label>
+
+                            <input type="date" id="new_date" name="new_date" value="<?= $task['task_date'] ?>" min="<?= $actualDateString ?>">
                             <input type="submit" value="✒️">
                         </form>
                     </div>
                 </div>
-                <div class="remind_date">
-                <form action="components/task/task_actions.php" method="POST">
-                    <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
-                    <input type="hidden" name="id_task" value="<?= $task['id_task'] ?>">
-                    <input type="hidden" name="action" value="date">
-                    <label for="new_date">Date de rappel:</label>
+                <div class="right__container">
 
-                    <input type="date" id="new_date" name="new_date" value="<?= $task['task_date'] ?>" min="<?= $task['task_date'] ?>">
-                    <input type="submit" value="✒️">
-                </form>
-                </div>
-            </div>
-            <div class="right__container">
-
-                <div class="right__container--arrows">
-                    <p class="btn up_btn">
-                        <a href="components/task/change_order.php?action=up&id=<?= $task['id_task'] ?>">⬆️</a>
-                    </p>
-                    <p class="task__action">
-                    <div class="btn">
-                        <a href="components/task/task_actions.php?action=delete&token=<?= $_SESSION['token'] ?>&id=<?= $task['id_task'] ?>">🗑️</a>
+                    <div class="right__container--arrows">
+                        <p class="btn up_btn">
+                            <a href="components/task/change_order.php?action=up&id=<?= $task['id_task'] ?>">⬆️</a>
+                        </p>
+                        <p class="task__action">
+                        <div class="btn">
+                            <a href="components/task/task_actions.php?action=delete&token=<?= $_SESSION['token'] ?>&id=<?= $task['id_task'] ?>">🗑️</a>
+                        </div>
+                        </p>
+                        <p class="btn down_btn">
+                            <a href="components/task/change_order.php?action=down&id=<?= $task['id_task'] ?>">⬇️</a>
+                        </p>
                     </div>
-                    </p>
-                    <p class="btn down_btn">
-                        <a href="components/task/change_order.php?action=down&id=<?= $task['id_task'] ?>">⬇️</a>
-                    </p>
                 </div>
-            </div>
-        </li>
-    </ul>
-    <?php } } else { ?>
+            </li>
+        </ul>
+    <?php }
+} else { ?>
     <div>
         <p class="youpi">🥳</p>
         <h2>YOUPIIIIII !! y'a plus rien à faire !!</h2>
